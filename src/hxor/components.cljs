da37a6358f9617cf680968c8777fdbf47dbecae1
Josef Pospíšil <josef.pospisil@laststar.eu>
2018-11-07 11:58:27 +0100

    Remove hiccup/parse calls and fix Number ns

--- src/hxor/components.cljs
+++ src/hxor/components.cljs
@@ -9,27 +9,26 @@
 (defn cursor [state key] (get (deref state) key))
 
 (hx/defnc Header [{:keys [kind] :as props}]
-  (hiccup/parse
-   [:header
-    {:style {:padding "0 2rem"}}
-    [:h1 "Hello, you "
-     [:span {:style {:font-weight "bold"}} kind] " HXOR!"]]))
+  [:header
+   {:style {:padding "0 2rem"}}
+   [:h1 "Hello, you "
+    [:span {:style {:font-weight "bold"}} kind] " HXOR!"]])
 
 (hx/defnc Buttons [{:keys [kind store]}]
   [:div
    (for [k    ["nice" "leet"]
          :let [current (= kind k)]]
-     (hiccup/parse [:button {:key (str (when current "disabled-" )kind)
-                             :disabled current
-                             :on-click #(ptk/emit! store (events/->Make k))}
-                    "make HXOR " k]))])
+     [:button {:key (str (when current "disabled-" )kind)
+               :disabled current
+               :on-click #(ptk/emit! store (events/->Make k))}
+      "make HXOR " k])])
 
 (hx/defnc Counting [{:keys [count store]}]
   [:div
    [:label "HXOR must count to: "
     [:input {:value count
-             :on-change #(ptk/emit! store (events/->SetCount (-> % .-target .-value Number.parseInt)))}]]
-   [:div (map #(hiccup/parse [:span {:key (str %)} % " "]) (range 1 (inc count)))]])
+             :on-change #(ptk/emit! store (events/->SetCount (-> % .-target .-value js/Number.parseInt)))}]]
+   [:div (map (fn [i] [:span {:key (str i)} i " "]) (range 1 (inc count)))]])
 
 (hx/defnc Main [{:keys [count store kind]}]
   [:main
